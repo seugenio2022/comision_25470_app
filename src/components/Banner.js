a802928289a9646bbf084b1c32feccbac0e584eb
Sebastian Eugenio <sebas-cs1.6@hotmail.com>
2022-03-30 20:54:26 -0300

    add carousel banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,70 @@
+import * as React from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MobileStepper from '@mui/material/MobileStepper';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
 
-export default function Banner({ show }) {
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const images = [
+	{
+		imgPath: '/banner1.png',
+	},
+	{
+		imgPath: '/banner2.png',
+	}
+];
+
+export default function Banner() {
+	const theme = useTheme();
+	const [activeStep, setActiveStep] = React.useState(0);
+	const maxSteps = images.length;
+
+	const handleNext = () => {
+		setActiveStep((prevActiveStep) => prevActiveStep + 1);
+	};
+
+	const handleBack = () => {
+		setActiveStep((prevActiveStep) => prevActiveStep - 1);
+	};
+
+	const handleStepChange = (step) => {
+		setActiveStep(step);
+	};
+
 	return (
-		show ?
-			<img height="150px" width="100%" src="/banner.png" />
-			: <></>
-	)
+		<Box sx={{ width: "100%" }}>
+			<AutoPlaySwipeableViews
+				axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+				index={activeStep}
+				onChangeIndex={handleStepChange}
+				enableMouseEvents
+			>
+				{images.map((step, index) => (
+					<div key={step.label}>
+						{Math.abs(activeStep - index) <= 2 ? (
+							<Box
+								component="img"
+								sx={{
+									height: 300,
+									display: 'block',
+									overflow: 'hidden',
+									width: '100%',
+								}}
+								src={step.imgPath}
+							/>
+						) : null}
+					</div>
+				))}
+			</AutoPlaySwipeableViews>
+		</Box>
+	);
 }
+
+
